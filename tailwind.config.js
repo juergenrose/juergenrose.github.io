e56dbc6186a029c1b42cecdd855aa362833bb47a
juergen mosgan <juergen.mosgan@edu.fh-kaernten.ac.at>
2024-12-04 22:29:08 +0100

    changed content settings and added gitignore file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{html,js}",
+    "index.html",
+    "data-privacy.html",
+    "imprint.html",
+  ],
   theme: {
     extend: {},
   },
