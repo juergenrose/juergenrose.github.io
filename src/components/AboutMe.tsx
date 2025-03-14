9e6189e0d1edd4ef526f7ca0e081b619449524d4
Jürgen Mosgan <j.mosgan@gmail.com>
2025-03-14 19:23:23 +0100

    created AboutMe component with styling

---
+++ src/components/AboutMe.tsx
@@ -0,0 +1,15 @@
+export default function AboutMe() {
+  return (
+    <section className="flex flex-col items-center justify-center content-center w-[800px] mx-auto mt-16">
+      <h1 className="aboutme-heading">Über mich</h1>
+      <p className="aboutme-paragraph">
+        Web-Entwicklung ist f&uuml;r mich mehr als nur ein Beruf - es ist eine
+        Leidenschaft, die mich antreibt, benutzerfreundliche und &auml;sthetisch
+        ansprechende Webanwendungen zu gestalten. Ebenso fasziniert mich die
+        Welt von Linux, deren Offenheit und Flexibilit&auml;t mir immer wieder
+        neue M&ouml;glichkeiten er&ouml;ffnet und meine Begeisterung für
+        Technologie weiter entfacht.
+      </p>
+    </section>
+  );
+}
