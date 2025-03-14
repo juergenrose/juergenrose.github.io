94d7d0f8ca288e3745e56f983db2cfcbc8184f63
Jürgen Mosgan <j.mosgan@gmail.com>
2025-03-14 19:20:07 +0100

    created Header component with styling

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <header>
+      <nav className="nav">
+        <ul className="nav-bar">
+          <li className="nav-link">Über mich</li>
+          <li className="nav-link">Berufserfahrung</li>
+          <li className="nav-link">Kontakt</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
