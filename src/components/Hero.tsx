3e03b888c0b35ebe7720175c7ce8e51977df48d1
Jürgen Mosgan <j.mosgan@gmail.com>
2025-03-16 19:51:34 +0100

    addded portrait image and added it in Hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,25 @@
 export default function Hero() {
   return (
-    <section className="flex flex-col justify-center items-center py-40 space-y-4 bg-gray-950 border-b-2 border-violet-400">
-      <h1 className="hero-heading">
-        Hallo, mein Name ist <span className="hero-span">Jürgen Mosgan</span>.
-      </h1>
-      <p className="hero-paragraph">Webentwickler und Linux Enthusiast</p>
-      <button className="hero-btn">Kontakt aufnehmen</button>
+    <section className="flex justify-center pt-40 bg-gray-950 border-b-2 border-violet-400">
+      <div className="w-2/3 grid grid-cols-2 gap-0">
+        <div className="flex content-center items-center">
+          <img
+            src="/src/assets/myself.png"
+            alt="Bild des Webseiten Erstellers"
+            className="w-150 h-auto"
+          />
+        </div>
+        <div className="flex flex-col gap-4 my-auto">
+          <h1 className="hero-heading">
+            Hallo, mein Name ist
+            <span className="hero-span">Jürgen Mosgan</span>.
+          </h1>
+          <p className="hero-paragraph">Webentwickler und Linux Enthusiast</p>
+          <a href="#aboutme" className="hero-btn">
+            Mehr erfahren
+          </a>
+        </div>
+      </div>
     </section>
   );
 }
