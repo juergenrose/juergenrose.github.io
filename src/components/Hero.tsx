1a69a7c62eeb6b5b1d63929b761d0a3f70129c99
Juergen Mosgan <j.mosgan@gmail.com>
2025-07-28 19:52:37 +0200

    hero width bugfix for mobile devices

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ export default function Hero() {
   return (
     <section className="flex justify-center items-center content-center px-4 md:pt-40 pt-10 bg-gray-950 border-b-2 border-violet-400">
       <div className="grid md:grid-cols-2 grid-cols-1 gap-0 ">
-        <div className="flex content-center items-center mx-auto w-[600px]">
+        <div className="flex content-center items-center mx-auto md:w-[600px]">
           <img
             src={portraitImage}
             alt="Bild des Webseiten Erstellers"
@@ -12,7 +12,7 @@ export default function Hero() {
           />
           ;
         </div>
-        <div className="flex flex-col gap-2 my-auto justify-center w-[600px] content-center items-center text-center">
+        <div className="flex flex-col gap-2 my-auto justify-center md:w-[600px] content-center items-center text-center">
           <h1 className="hero-heading">
             Hallo, mein Name ist{" "}
             <span className="hero-span">Jürgen Mosgan</span>.
