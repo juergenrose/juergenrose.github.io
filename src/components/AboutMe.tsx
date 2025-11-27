81cede0113dd2e5f8a8c49f7954503e628116f38
Jürgen M <j.mosgan@proton.me>
2025-11-27 16:03:10 +0000

    Update file AboutMe.tsx

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -6,12 +6,11 @@ export default function AboutMe() {
     >
       <h1 className="aboutme-heading">Über mich</h1>
       <p className="aboutme-paragraph">
-        Web-Entwicklung ist f&uuml;r mich mehr als nur ein Beruf - es ist eine
-        Leidenschaft, die mich antreibt, benutzerfreundliche und &auml;sthetisch
-        ansprechende Webanwendungen zu gestalten. Ebenso fasziniert mich die
-        Welt von Linux, deren Offenheit und Flexibilit&auml;t mir immer wieder
-        neue M&ouml;glichkeiten er&ouml;ffnet und meine Begeisterung für
-        Technologie weiter entfacht.
+        Softwareentwicklung ist f&uuml;r mich mehr als nur ein Beruf - es ist eine
+        Leidenschaft, die mich antreibt, benutzerfreundliche, effiziente und durchdachte
+        Anwendungen zu gestalten. Ebenso fasziniert mich die Welt von Linux, deren 
+        Offenheit und Flexibilit&auml;t mir immer wieder neue M&ouml;glichkeiten 
+        er&ouml;ffnet und meine Begeisterung für Technologie weiter entfacht.
       </p>
     </section>
   );
