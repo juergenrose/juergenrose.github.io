7b66dff6d7fd28491408e75240f0387c4b52781f
juergen mosgan <j.mosgan@proton.me>
2025-12-15 22:50:10 +0100

    added experience and aboutme tags, added email entity

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -9,11 +9,23 @@ export default function AboutMe() {
       id="aboutme"
     >
       <div className="flex mb-6">
-              <ul className="flex gap-2">
-                <li className="experience-tools-tag">Software Entwickler</li>
-                <li className="experience-tools-tag">Linux Enthusiast</li>
-              </ul>
-            </div>
+        <ul className="flex gap-2">
+          <li className="aboutme-tools-tag">Software Entwickler</li>
+          <li className="aboutme-tools-tag">Linux Enthusiast</li>
+        </ul>
+      </div>
+      <div className="flex mb-6">
+        <ul className="tools-list">
+          <li className="experience-tools-tag">HTML5</li>
+          <li className="experience-tools-tag">CSS3</li>
+          <li className="experience-tools-tag">TailwindCSS</li>
+          <li className="experience-tools-tag">JavaScript</li>
+          <li className="experience-tools-tag">Java</li>
+          <li className="experience-tools-tag">Spring Boot</li>
+          <li className="experience-tools-tag">MySQL</li>
+          <li className="experience-tools-tag">Linux</li>
+        </ul>
+      </div>
       <p className="aboutme-paragraph">
         Softwareentwicklung ist für mich mehr als nur ein Beruf – sie ist meine
         Leidenschaft, benutzerfreundliche und effiziente Anwendungen zu entwickeln. 
@@ -36,7 +48,7 @@ export default function AboutMe() {
             className="w-16 h-16 rounded-full mx-auto py-4 bg-gray-200 border border-gray-800"
           />
         </a>
-        <a href="mailto:j.mosgan@gmail.com"  className="bg-emerald-500 border-2 rounded-full p-1 hover:bg-gray-500 transition ease-in-out duration-300">
+        <a href="mailto:&#106;&#46;&#109;&#111;&#115;&#103;&#97;&#110;&#64;&#112;&#114;&#111;&#116;&#111;&#110;&#46;&#109;&#101;"  className="bg-emerald-500 border-2 rounded-full p-1 hover:bg-gray-500 transition ease-in-out duration-300">
           <img
             src={mailIcon}
             alt="Mail Icon"
