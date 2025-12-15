7e15aa06da4ce820c4500fdf5507efc2b1513886
juergen mosgan <j.mosgan@proton.me>
2025-12-15 18:51:43 +0100

    removed old nav from privacy and imprint page, color changes, email is now in entity format

--- src/components/Imprint.tsx
+++ src/components/Imprint.tsx
@@ -1,36 +1,23 @@
-import { Link } from "@tanstack/react-router";
-import homeIcon from "../assets/icons/home.svg";
-
 export default function Imprint() {
   return (
-    <section className="flex flex-col gap-1 md:mx-auto px-4 items-start justify-center content-start md:w-[800px] md:mt-20 mt-10">
-      <div className="flex justify-center mx-auto gap-5">
-        <Link to="/" className="home-btn">
-          <img src={homeIcon} alt="zurück zur Startseite Button" />
-          Startseite
-        </Link>
-
-        <Link to="/privacy" className="home-btn">
-          Datenschutz
-        </Link>
-      </div>
-
+    <section className="flex flex-col gap-1 md:mx-auto px-4 items-start justify-center content-start md:w-[800px]">
       <h2 className="imprint-heading">Impressum</h2>
       <h3 className="imprint-middle-font">
-        Informationen und Offenlegung gemäß §5 (1) ECG, § 25 MedienG, § 63 GewO
+        Informationen und Offenlegung gem&auml;ß §5 (1) ECG, § 25 MedienG, § 63 GewO
         und § 14 UGB
       </h3>
       <h3 className="imprint-middle-font mt-5">Name des Website-Betreibers:</h3>
-      <p className="imprint-paragraph-font">Jürgen Mosgan</p>
+      <p className="imprint-paragraph-font">J&uuml;rgen Mosgan</p>
       <h3 className="imprint-middle-font mt-5">Kontakt:</h3>
       <p className="imprint-paragraph-font">
-        <b className="text-bold text-teal-500">E-Mail:</b> j.mosgan@gmail.com
+        <b className="text-bold text-emerald-500">E-Mail:</b> &#106;&#46;&#109;&#111;&#115;&#103;&#97;&#110;&#64;&#112;&#114;&#111;&#116;&#111;&#110;&#46;&#109;&#101;
+
       </p>
       <h3 className="imprint-middle-font mt-5">
         Hinweis zur Verantwortlichkeit:
       </h3>
       <p className="imprint-paragraph-font">
-        Als Privatperson bin ich für die Inhalte auf dieser Website
+        Als Privatperson bin ich f&uuml;r die Inhalte auf dieser Website
         verantwortlich. Die hier pr&auml;sentierten Inhalte sind nach bestem
         Wissen und Gewissen erstellt.
       </p>
